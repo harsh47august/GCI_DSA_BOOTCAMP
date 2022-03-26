b65b6f96f6e8ac76abdd289cbd01fd3ed456fe90
harsh47august <98662467+harsh47august@users.noreply.github.com>
2022-03-27 03:10:59 +0530

    Add files via upload

---
+++ MILESTONE 14/factorial.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int ans = factorial( n);
+        System.out.println(ans);
+
+    }
+
+    public static int factorial(int n){
+        if( n == 0){
+            return 1;
+        }
+
+        return n*factorial(n-1);
+    }
+
+}
